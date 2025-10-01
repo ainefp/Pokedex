803b488d198aba62d769ace01b0b70f21fe1e63c
Ainé Figueroa <aine.xena.100@gmail.com>
2025-10-01 12:51:55 +0200

    añadí imágenes

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const statsList = document.getElementById("stats");
 const sonido = document.getElementById("sonido");
 const input = document.getElementById("pokeInput");
 const btn = document.getElementById("buscarBtn");
+const imgFront = document.getElementById("img-front");
+const imgBack = document.getElementById("img-back");
+
+// tabla
+const tabla = document.getElementsByTagName("tr");
 
 const div = document.getElementById("pokediv");
 // div.style.display = "none";
@@ -20,16 +25,23 @@ btn.addEventListener("click", () => {
     .then(response => response.json())
     .then(datos => {
         const name = datos.name;
+        const front_default = datos.sprites.front_default;
+        const back_default = datos.sprites.back_default;
         const stats = datos.stats;
         const cries = datos.cries.latest;
 
+        // no funciona actualmente
+        // en vez de añadirlo a la tabla, crear una segunda fila en la tabla
         stats.forEach(stat => {
-            const statLi = document.createElement("li");
-            statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+            for (const th of tabla) {
+                th.textContent = stat.base_stat;
+            }
             statsList.appendChild(statLi);
         });
 
-        nombre.textContent = name;
+        nombre.textContent = name.toUpperCase();
+        imgFront.src = front_default;
+        imgBack.src = back_default;
         sonido.src = cries;
     }).catch(error => console.log("Ha ocurrido un error tratando de obtener los datos: ", error));
 });
